b7c97a98b886e0c553f9c04caff058a77219c462
gozde.kaval@gmail.com <Fatistan.git123>
2018-05-09 23:33:37 +0300

    Remove import

--- app/src/main/java/com/smartlifecoach/itu/smartlifecoachforchildren/ChildrenDatabesePack/ChildrenDatabase.java
+++ app/src/main/java/com/smartlifecoach/itu/smartlifecoachforchildren/ChildrenDatabesePack/ChildrenDatabase.java
@@ -8,7 +8,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import com.smartlifecoach.itu.smartlifecoachforchildren.FoodModel.FoodDBEntry;
-import com.smartlifecoach.itu.smartlifecoachforchildren.FoodModel.FoodItem;
 import com.smartlifecoach.itu.smartlifecoachforchildren.SignIn.Child;
 
 import java.util.ArrayList;
